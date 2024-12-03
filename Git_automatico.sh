d5503c459e3670f91e1b7d37d2002c013fca0974
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2024-12-03 17:24:33 +0100

    Create Git_automatico.sh

---
+++ Git_automatico.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Directory da gestire
+DIR="DAM/"
+
+# Verifica se la directory esiste
+if [ -d "$DIR" ]; then
+  # Spostarsi nella directory
+  cd "$DIR" || exit
+
+  # Ottieni la data corrente in formato YYYY-MM-DD
+  CURRENT_DATE=$(date +%Y-%m-%d)
+
+  # Aggiungere tutti i file al controllo di versione
+  git add .
+
+  # Creare il commit con il messaggio che include la data corrente
+  git commit -m "Commit del giorno: $CURRENT_DATE"
+
+  # Eseguire il push dei cambiamenti
+  git push
+else
+  echo "La directory $DIR non esiste."
+fi
